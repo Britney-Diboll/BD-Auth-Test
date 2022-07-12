9cf8bb4095addf14b4e6484920904e5218d3d53e
Britney Diboll <36859804+Britney-Diboll@users.noreply.github.com>
2022-07-12 08:22:04 -0400

    test3

--- index.js
+++ index.js
@@ -1,41 +1,58 @@
 
 import{} from '@netlify/functions';
 
-netlifyIdentity.init({
-  container: '#netlify-modal', // defaults to document.body
-  locale: 'en' // defaults to 'en'
+
+netlifyIdentity.on('init', () => {
+  initUser = netlifyIdentity.currentUser();
+})
+
+netlifyIdentity.on('login', () => {
+  if (initUser == null) {
+      window.location.replace('dashboard');
+  }
+  netlifyIdentity.close();
 });
-// Open the modal
-netlifyIdentity.open();
+
+netlifyIdentity.on('logout', () => {
+  netlifyIdentity.close();
+  window.location.replace('/');
+});
+
+// netlifyIdentity.init({
+//   container: '#netlify-modal', // defaults to document.body
+//   locale: 'en' // defaults to 'en'
+// });
+// // Open the modal
+// netlifyIdentity.open();
 
 
 
-// Get the current user:
-// Available after on('init') is invoked
-const user = netlifyIdentity.currentUser();
+// // Get the current user:
+// // Available after on('init') is invoked
+// const user = netlifyIdentity.currentUser();
 
-// Bind to events
-netlifyIdentity.on('init', user => console.log('init', user));
-netlifyIdentity.on('login', user => console.log('login', user));
-netlifyIdentity.on('logout', () => console.log('Logged out'));
-netlifyIdentity.on('error', err => console.error('Error', err));
-netlifyIdentity.on('open', () => console.log('Widget opened'));
-netlifyIdentity.on('close', () => console.log('Widget closed'));
+// // Bind to events
+// netlifyIdentity.on('init', user => console.log('init', user));
+// netlifyIdentity.on('login', user => console.log('login', user));
+// netlifyIdentity.on('logout', () => console.log('Logged out'));
+// netlifyIdentity.on('error', err => console.error('Error', err));
+// netlifyIdentity.on('open', () => console.log('Widget opened'));
+// netlifyIdentity.on('close', () => console.log('Widget closed'));
 
-// Unbind from events
-netlifyIdentity.off('login'); // to unbind all registered handlers
-netlifyIdentity.off('login', handler); // to unbind a single handler
+// // Unbind from events
+// netlifyIdentity.off('login'); // to unbind all registered handlers
+// netlifyIdentity.off('login', handler); // to unbind a single handler
 
-// Close the modal
-netlifyIdentity.close();
+// // Close the modal
+// netlifyIdentity.close();
 
-// Log out the user
-netlifyIdentity.logout();
+// // Log out the user
+// netlifyIdentity.logout();
 
-// Refresh the user's JWT
-// Call in on('login') handler to ensure token refreshed after it expires (1hr)  
-// Note: this method returns a promise.
-netlifyIdentity.refresh().then((jwt)=>console.log(jwt))
+// // Refresh the user's JWT
+// // Call in on('login') handler to ensure token refreshed after it expires (1hr)  
+// // Note: this method returns a promise.
+// netlifyIdentity.refresh().then((jwt)=>console.log(jwt))
 
-// Change language
-netlifyIdentity.setLocale('en');
+// // Change language
+// netlifyIdentity.setLocale('en');
